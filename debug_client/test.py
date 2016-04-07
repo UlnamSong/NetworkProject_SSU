3a5af71d8473c1a6233e2288b73f82f3b2df0df7
nagi <akanagi95@gmail.com>
2016-04-07 15:48:29 +0900

    debug_client proto

--- debug_client/test.py
+++ debug_client/test.py
@@ -1,3 +1,10 @@
 from socket import *
 
+ip = 'localhost'
+port = 9090
 
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect((ip,port))
+s.send("\x00\x00\x00\x00\x00\x00\x00\x00\x41\x41\x41\x41")
+s.close()
